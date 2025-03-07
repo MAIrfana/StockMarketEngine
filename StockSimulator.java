b11c05adce237627ae79534ccebfb6ad4bd9f48b
MA Irfana <ilnu@pnw.edu>
2025-03-06 21:38:02 -0600

    orders randomized

---
+++ StockSimulator.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class StockSimulator {
+    public static final int TICKER_COUNT = 1024;
+    public static final Random RANDOM = new Random();
+    public static String [] TICKER;
+    static {
+        TICKER = new String[TICKER_COUNT];
+        for(int i =0;i<TICKER_COUNT;i++){
+            TICKER[i] = "STOCK"+(i+1);
+        }
+    }
+    public static void simulate(){
+        while(true){
+            Order.OrderType type = RANDOM.nextBoolean()? Order.OrderType.BUY: Order.OrderType.SELL;
+            String ticker = TICKER[RANDOM.nextInt(1024)];
+            int quantity = RANDOM.nextInt(100)+1;
+            double price = 10 + (490 * RANDOM.nextDouble());
+
+            System.out.println(ticker+" "+type+" "+" "+quantity+" "+price);
+        }
+    }
+}
